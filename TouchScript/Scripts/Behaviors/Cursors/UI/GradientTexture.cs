c0974c3e0083b6ed0f7bfea90adb8eee43d3a610
James Kim <eodnjs2998@gmail.com>
2024-05-07 12:39:32 +0900

    wip

--- TouchScript/Scripts/Behaviors/Cursors/UI/GradientTexture.cs
+++ TouchScript/Scripts/Behaviors/Cursors/UI/GradientTexture.cs
@@ -3,8 +3,8 @@
  */
 
 using System;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace TouchScript.Behaviors.Cursors.UI
@@ -12,119 +12,71 @@ namespace TouchScript.Behaviors.Cursors.UI
     /// <summary>
     /// Generates a texture with a circle gradient.
     /// </summary>
-    [HelpURL("http://touchscript.github.io/docs/html/T_TouchScript_Behaviors_Cursors_UI_GradientTexture.htm")]
-    public class GradientTexture : MonoBehaviour
+    [GraphicPropertyHide(GraphicPropertyFlag.Color | GraphicPropertyFlag.Raycast)]
+    public class GradientTexture : Graphic
     {
-        /// <summary>
-        /// Resolution in pixels.
-        /// </summary>
-        public enum Res
+        [FormerlySerializedAs("Gradient")]
+        [SerializeField]
+        Gradient _gradient;
+        [NonSerialized]
+        Texture2D _texture;
+
+        public override Texture mainTexture
         {
-            /// <summary>
-            /// 16x16
-            /// </summary>
-            Pix16 = 16,
-
-            /// <summary>
-            /// 32x32
-            /// </summary>
-            Pix32 = 32,
-
-            /// <summary>
-            /// 64x64
-            /// </summary>
-            Pix64 = 64,
-
-            /// <summary>
-            /// 128x128
-            /// </summary>
-            Pix128 = 128,
-
-            /// <summary>
-            /// 256x256
-            /// </summary>
-            Pix256 = 256,
-
-            /// <summary>
-            /// 512x512
-            /// </summary>
-            Pix512 = 512
+            get
+            {
+                if (_texture is null) Refresh();
+                return _texture;
+            }
         }
 
-        /// <summary>
-        /// The gradient.
-        /// </summary>
-        public Gradient Gradient = new Gradient();
+        void Start() => Refresh();
 
-        /// <summary>
-        /// Gradient's name. Used to cache textures.
-        /// </summary>
-        public string Name = "Gradient";
-
-        /// <summary>
-        /// Texture resolution.
-        /// </summary>
-        public Res Resolution = Res.Pix128;
-
-        private Texture2D texture;
-        private static Dictionary<int, Texture2D> textureCache = new Dictionary<int, Texture2D>();
-
-        /// <summary>
-        /// Generates the gradient texture.
-        /// </summary>
-        /// <returns>Generated texture.</returns>
-        public Texture2D Generate()
+        void OnDestroy()
         {
-            var res = (int) Resolution;
-            var tex = new Texture2D(res, 1, TextureFormat.ARGB32, false, true)
+            if (_texture is not null)
             {
-                name = Name,
-                filterMode = FilterMode.Bilinear,
-                wrapMode = TextureWrapMode.Clamp
-            };
+                DestroyImmediate(_texture);
+                _texture = null;
+            }
+        }
 
+        void Refresh()
+        {
+            _texture ??= CreateTexture();
+            ApplyGradient(_texture, _gradient);
+        }
+
+        protected override void OnPopulateMesh(MeshBuilder mb)
+        {
+            var r = rectTransform.rect;
+            var pos1 = r.min;
+            var pos2 = r.max;
+            mb.SetUp_Quad_FullUV(pos1, pos2, color);
+        }
+
+        static Texture2D CreateTexture()
+        {
+            return new Texture2D(128, 1, TextureFormat.ARGB32, false)
+            {
+                name = "",
+                filterMode = FilterMode.Bilinear,
+                wrapMode = TextureWrapMode.Clamp,
+                hideFlags = HideFlags.DontSave // XXX: To prevent it from being destroyed when the scene is reloaded.
+            };
+        }
+
+        static void ApplyGradient(Texture2D tex, Gradient gradient)
+        {
+            var res = tex.width;
             var colors = new Color[res];
-            float div = res;
             for (var i = 0; i < res; i++)
             {
-                float t = i / div;
-                colors[i] = Gradient.Evaluate(t);
+                var t = i / (float) res;
+                colors[i] = gradient.Evaluate(t);
             }
             tex.SetPixels(colors);
             tex.Apply(false, true);
-
-            return tex;
-        }
-
-        private void Start()
-        {
-            var hash = Name.GetHashCode();
-            if (!textureCache.TryGetValue(hash, out texture) || texture == null)
-            {
-                texture = Generate();
-                textureCache[hash] = texture;
-            }
-            apply();
-        }
-
-        private void OnValidate()
-        {
-            refresh();
-        }
-
-        private void refresh()
-        {
-            if (texture != null)
-                DestroyImmediate(texture);
-            texture = Generate();
-            apply();
-        }
-
-        private void apply()
-        {
-            var r = GetComponent<RawImage>();
-            if (r == null) throw new Exception("GradientTexture must be on an UI element with RawImage component.");
-            r.texture = texture;
         }
     }
 }
